bde2e1e0064239dc1053a951b475683acb1e9e67
kk0055 <rtsw.lgy@gmail.com>
2021-01-19 14:47:41 +0800

    chat管理者画面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,93 @@
     <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
     <script src=“https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js”></script>
     
+
+    <script>
+        var receiver_id = '';
+        var my_id = "{{ Auth::id() }}";
+        $(document).ready(function () {
+            // ajax setup form csrf token
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
+            var pusher = new Pusher('f215ae5857618ed02fd0', {
+                  cluster  : 'ap3',
+                  encrypted: true
+              });
+      
+            //購読するチャンネルを指定
+              var Channel = pusher.subscribe('chat');
+      
+              Channel.bind('chat_event', function (data) {
+                // alert(JSON.stringify(data));
+                if (my_id == data.send) {
+                    $('#' + data.receive).click();
+                } else if (my_id == data.receive) {
+                    if (receiver_id == data.send) {
+                        // if receiver is selected, reload the selected user ...
+                        $('#' + data.send).click();
+                    } else {
+                        // if receiver is not seleted, add notification for that user
+                        var pending = parseInt($('#' + data.send).find('.pending').html());
+                        if (pending) {
+                            $('#' + data.send).find('.pending').html(pending + 1);
+                        } else {
+                            $('#' + data.send).append('<span class="pending">1</span>');
+                        }
+                    }
+                }
+            });
+            $('.user').click(function () {
+                $('.user').removeClass('active');
+                $(this).addClass('active');
+                $(this).find('.pending').remove();
+                receiver_id = $(this).attr('id');
+                $.ajax({
+                    type: "get",
+                    url: "message/" + receiver_id, // need to create this route
+                    data: "",
+                    cache: false,
+                    success: function (data) {
+                        $('#messages').html(data);
+                        scrollToBottomFunc();
+                    }
+                });
+            });
+            $(document).on('keyup', '.input-text input', function (e) {
+                var message = $(this).val();
+                // check if enter key is pressed and message is not null also receiver is selected
+                if (e.keyCode == 13 && message != '' && receiver_id != '') {
+                    $(this).val(''); // while pressed enter text box will be empty
+                    var datastr = "receiver_id=" + receiver_id + "&message=" + message;
+                    $.ajax({
+                        type: "post",
+                        url: "message", // need to create this post route
+                        data: datastr,
+                        cache: false,
+                        success: function (data) {
+                        },
+                        error: function (jqXHR, status, err) {
+                        },
+                        complete: function () {
+                            scrollToBottomFunc();
+                        }
+                    })
+                }
+            });
+        });
+        // make a function to scroll down auto
+        function scrollToBottomFunc() {
+            $('.message-wrapper').animate({
+                scrollTop: $('.message-wrapper').get(0).scrollHeight
+            }, 50);
+        }
+      </script>
+
+
     <script type="text/javascript">
 
 //ログを有効にする
