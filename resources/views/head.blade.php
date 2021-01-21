566349dd2e599da39b5d9b4f045d15cb5da359ab
kk0055 <rtsw.lgy@gmail.com>
2021-01-21 17:38:12 +0800

    Logo

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,16 +1,18 @@
 <section class="showcase " style="min-height: 90vh;">
   <header>
     
-    <h2 class="logo">Kasih</h2>
- 
+    {{-- <h2 class="logo">Kasih</h2> --}}
+    <div class="logo-image">
+   
+  </div>
     <div class="toggle"></div>
   </header>
   <img
-  class=" img w-full h-full bg-center bg-cover"
+  class="img w-full h-full bg-center bg-cover"
   style='background-image: url("https://images.unsplash.com/photo-1609986826541-063f1aa74219?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8NnNNVmpUTFNrZVF8fGVufDB8fHw%3D0");background-position:right bottom '
 >
   <div class="overlay"></div>
-  <div class="text">
+  <div class="text mt-5">
     
     <h3>We make a living by what we get.</h3>
     <h3>But we make a life by what we give.</h3>
