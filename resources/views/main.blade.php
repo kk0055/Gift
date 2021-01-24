bff3a9bbe7913fd4c866c1907d117d88ea458c88
kk0055 <rtsw.lgy@gmail.com>
2021-01-24 22:08:21 +0800

    Sakura server

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,7 +26,7 @@
                   <a href="{{ route('item.show',['itemId'=> $item->id]) }}">
                     @if ($item->image)
                       <img src="storage/image/{{ $item->image }}" alt="pic" class="hover:opacity-75 transition ease-in-out duration-150">
-                      <img src="{{asset('storage/public/'.$item->image)  }}" alt="pic" class="hover:opacity-75 transition ease-in-out duration-150">
+                      <img src="{{asset('storage/image/'.$item->image)  }}" alt="pic" class="hover:opacity-75 transition ease-in-out duration-150">
                       @else
                       <img class="no-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" alt="No Picture" class="hover:opacity-75 transition ease-in-out duration-150">  
                       @endif
