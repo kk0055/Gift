d3b6b7f7f0e6323a0854ae072aa09991c17f30c9
kk0055 <rtsw.lgy@gmail.com>
2021-01-24 18:44:41 +0800

    Sakura

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -25,7 +25,7 @@
                 <div class="append mt-8 ">
                   <a href="{{ route('item.show',['itemId'=> $item->id]) }}">
                     @if ($item->image)
-                      <img src="../storage/image/{{ $item->image }}" alt="pic" class="hover:opacity-75 transition ease-in-out duration-150">
+                      <img src="/storage/image/{{ $item->image }}" alt="pic" class="hover:opacity-75 transition ease-in-out duration-150">
                       <img src="{{asset($item->image)  }}" alt="pic" class="hover:opacity-75 transition ease-in-out duration-150">
                       @else
                       <img class="no-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png" alt="No Picture" class="hover:opacity-75 transition ease-in-out duration-150">  
